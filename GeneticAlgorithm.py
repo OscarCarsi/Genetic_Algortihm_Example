390d989de00f3c6d66f17255963cfd8186f7e0b6
OscarCarsi <oscarolivarezcarsi@gmail.com>
2024-04-17 16:08:12 -0600

    Complete crossover and remplace

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -29,19 +29,51 @@ def selection_by_roulette(population):
     #Sumamos las aptitudes
     total_fitness = sum(fitnesses)
     #Creamos una lista con las probabilidades de cada individuo, dividiendo la aptitud de cada individuo entre la aptitud total
-    probabilities = [f/total_fitness for f in fitnesses]
-    print("Probabilities: ", probabilities)
+    probabilities = [round(f/total_fitness,2) for f in fitnesses]
     #Seleccionamos a los individuos, donde le pasamos la población y las probabilidades, y seleccionamos 2 individuos
     selected = random.choices(population, probabilities, k=2)
     return selected
 
+#Cruzamos los padres con blx_alpha
+def blx_alpha_crossover(parent1, parent2, alpha=0.5):
+    child1, child2 = [], [] #se crean los arrays para los hijos
+    for p1, p2 in zip(parent1, parent2): #Se itera sobre los valores de cada padre
+        cmin = max(min(p1, p2) - alpha * abs(p1 - p2), -5.12) #Se calcula el valor mínimo
+        cmax = min(max(p1, p2) + alpha * abs(p1 - p2), 5.12) #Se calcula el valor máximo
+        child1.append(round(random.uniform(cmin, cmax),2)) #Se añade un valor aleatorio entre el mínimo y el máximo al hijo 1
+        child2.append(round(random.uniform(cmin, cmax),2)) #Se añade un valor aleatorio entre el mínimo y el máximo al hijo 2
+    return child1, child2 
+
+def replacement_with_elitism(population, parents, children):
+    # Convierte children en una lista 
+    children = list(children)
+    # Combina los padres y los hijos
+    combined = parents + children
+    
+    # Calcula la aptitud de cada individuo con los padres y los hijos
+    fitnesses = [(individual, fitness(individual)) for individual in combined]
+
+    # Ordena los individuos por aptitud en orden ascendente
+    sorted_by_fitness = sorted(fitnesses, key=lambda x: x[1])
+
+    # Selecciona los dos mejores individuos
+    best_two = [individual for individual, _ in sorted_by_fitness[:2]]
+
+    # Reemplaza los padres en la población original con los dos mejores individuos
+    for i in range(len(population)):
+        if population[i] in parents:
+            population[i] = best_two.pop(0)
+            if not best_two:
+                break
+
+    return population
+
 def main():
     population = create_population(5)
-    for individual in population:
-        print("Individual: ", individual)
-        print("Fitness: ", fitness(individual))
-    selected = selection_by_roulette(population)
-    print("Selected: ", selected)
+    parents = selection_by_roulette(population)
+    children = blx_alpha_crossover(parents[0], parents[1])
+    new_population = replacement_with_elitism(population, parents, children)
+
 
 if __name__ == "__main__":
     main()
